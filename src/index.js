1000e78467131630604c4c9d76ab76534d94e28d
DmitryNikolin <ni-ko-dim@ukr.net>
2022-11-05 12:34:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,6 +13,8 @@ const loadBtn = document.querySelector('.load-more');
 const closeBtn = document.querySelector('.close-btn');
 
 let perPage = 40;
+let perPageForInfinityScroll = Math.trunc(perPage / 5);
+let scrollPos = 0;
 let page = 0;
 let name = searchQuery.value.trim();
 
@@ -21,16 +23,6 @@ closeBtn.style.display = 'none';
 searchBtn.style.visibility = 'hidden';
 
 searchQuery.addEventListener('input', searchBtnAdd);
-
-// searchQuery.onkeydown = function onEnterPress(e) {
-//   searchStrLength = e.target.value.trim().length;
-//   keyCodePress = e.keyCode;
-//   if (keyCodePress === 13 && searchStrLength === 0) {
-//     Notiflix.Notify.failure(
-//       'You must enter at least one character in the search string. Repeat request please...'
-//     );
-//   }
-// };
 
 function searchBtnAdd(e) {
   let searchStr = e.target.value.trim();
@@ -54,7 +46,9 @@ async function fetchImages(name, page, perPage) {
     );
     return response.data;
   } catch (error) {
-    console.log('ERROR: ' + error);
+    Notiflix.Notify.failure('Add images error! ' + error);
+    // console.log('ERROR: ' + error);
+    return response.error;
   }
 }
 
@@ -70,7 +64,7 @@ async function eventHandler(e) {
     .then(name => {
       let totalPages = name.totalHits / perPage;
       let searchStrLength = searchQuery.value.trim().length;
-      
+
       if (searchStrLength > 0) {
         if (name.hits.length > 0) {
           Notiflix.Notify.success(`Hooray! We found ${name.totalHits} images.`);
@@ -105,7 +99,9 @@ async function eventHandler(e) {
         );
       }
     })
-    .catch(error => console.log('ERROR: ' + error));
+    .catch(error =>
+      Notiflix.Notify.failure('Error add images to gallery!' + error)
+    );
 }
 
 searchForm.addEventListener('submit', eventHandler);
@@ -153,55 +149,80 @@ function renderGallery(name) {
   gallery.insertAdjacentHTML('beforeend', markup);
 }
 
-loadBtn.addEventListener(
-  'click',
+// loadBtn.addEventListener(
+//   'click',
+//   () => {
+//     name = searchQuery.value;
+//     page += 1;
+//     fetchImages(name, page, perPage).then(name => {
+//       let totalPages = name.totalHits / perPage;
+//       renderGallery(name);
+//       new SimpleLightbox('.gallery a');
+//       if (page >= totalPages) {
+//         loadBtn.style.display = 'none';
+//         Notiflix.Notify.info(
+//           "We're sorry, but you've reached the end of search results."
+//         );
+//       }
+//     });
+//   },
+//   true
+// );
+
+window.addEventListener(
+  'scroll',
   () => {
-    name = searchQuery.value;
-    page += 1;
-    fetchImages(name, page, perPage).then(name => {
-      let totalPages = name.totalHits / perPage;
-      renderGallery(name);
-      new SimpleLightbox('.gallery a');
-      if (page >= totalPages) {
-        loadBtn.style.display = 'none';
-        Notiflix.Notify.info(
-          "We're sorry, but you've reached the end of search results."
-        );
-      }
-    });
+    if (document.body.getBoundingClientRect().top <= scrollPos) {
+      name = searchQuery.value;
+      page += 1;
+      loadBtn.style.display = 'none';
+      closeBtn.style.display = 'none';
+      perPage = perPageForInfinityScroll;
+      fetchImages(name, page, perPage).then(name => {
+        let totalPages = name.totalHits / perPage;
+        renderGallery(name);
+        new SimpleLightbox('.gallery a');
+        if (page >= totalPages) {
+          Notiflix.Notify.info(
+            "We're sorry, but you've reached the end of search results."
+          );
+        }
+      });
+      scrollPos = document.body.getBoundingClientRect().top;
+    }
   },
   true
 );
 
-let hintElem;
+// let hintElem;
 
-document.onmouseover = function (event) {
-  let target = event.target;
-  let hintHtml = target.dataset.hint;
-  if (!hintHtml) return;
+// document.onmouseover = function (event) {
+//   let target = event.target;
+//   let hintHtml = target.dataset.hint;
+//   if (!hintHtml) return;
 
-  hintElem = document.createElement('div');
-  hintElem.className = 'hint';
-  hintElem.innerHTML = hintHtml;
-  document.body.append(hintElem);
+//   hintElem = document.createElement('div');
+//   hintElem.className = 'hint';
+//   hintElem.innerHTML = hintHtml;
+//   document.body.append(hintElem);
 
-  let coords = target.getBoundingClientRect();
+//   let coords = target.getBoundingClientRect();
 
-  let left = coords.left + (target.offsetWidth - hintElem.offsetWidth) / 2;
-  if (left < 0) left = 0;
+//   let left = coords.left + (target.offsetWidth - hintElem.offsetWidth) / 2;
+//   if (left < 0) left = 0;
 
-  let top = coords.top - hintElem.offsetHeight - 5;
-  if (top < 0) {
-    top = coords.top + target.offsetHeight + 5;
-  }
+//   let top = coords.top - hintElem.offsetHeight - 5;
+//   if (top < 0) {
+//     top = coords.top + target.offsetHeight + 5;
+//   }
 
-  hintElem.style.left = left + 'px';
-  hintElem.style.top = top + 'px';
-};
+//   hintElem.style.left = left + 'px';
+//   hintElem.style.top = top + 'px';
+// };
 
-document.onmouseout = function (e) {
-  if (hintElem) {
-    hintElem.remove();
-    hintElem = null;
-  }
-};
+// document.onmouseout = function (e) {
+//   if (hintElem) {
+//     hintElem.remove();
+//     hintElem = null;
+//   }
+// };
